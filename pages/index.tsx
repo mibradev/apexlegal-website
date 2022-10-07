fc298e8b64055be3b2abddc42ab232b04724f77a
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2022-10-07 16:47:52 +0200

    Format

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,15 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Apex Legal</title>
-        <meta name="description" content="Translation and language solutions." />
+        <meta
+          name="description"
+          content="Translation and language solutions."
+        />
       </Head>
-
       <div className="bg-gradient-to-r from-cyan-900 to-cyan-700 text-cyan-100">
         <Header />
         <Hero />
       </div>
-
       <Features />
     </>
   )
@@ -31,8 +32,16 @@ function Hero() {
   return (
     <main className="p-6 space-y-6 py-24 text-center">
       <h1 className="text-3xl text-bold">Lorem ipsum dolor sit.</h1>
-      <p className="text-lg text-cyan-200">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus saepe nesciunt asperiores itaque amet quas.</p>
-      <a href="#" className="inline-block bg-gradient-to-r from-cyan-700 to-cyan-500 text-cyan-100 px-8 py-3 rounded-md text-xl text-bold shadow-md">Contact us</a>
+      <p className="text-lg text-cyan-200">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus saepe
+        nesciunt asperiores itaque amet quas.
+      </p>
+      <a
+        href="#"
+        className="inline-block bg-gradient-to-r from-cyan-700 to-cyan-500 text-cyan-100 px-8 py-3 rounded-md text-xl text-bold shadow-md"
+      >
+        Contact us
+      </a>
     </main>
   )
 }
@@ -41,28 +50,56 @@ function Features() {
   return (
     <section className="p-6 grid gap-6 md:grid-cols-2 xl:grid-cols-3">
       <div className="space-y-2">
-        <h1 className="text-xl text-bold text-cyan-900">A Powerful Human Touch</h1>
-        <p>Your words are personal. We respect that. So, your words stay in human hands from beginning to end. Your documents are human translated & human approved.</p>
+        <h1 className="text-xl text-bold text-cyan-900">
+          A Powerful Human Touch
+        </h1>
+        <p>
+          Your words are personal. We respect that. So, your words stay in human
+          hands from beginning to end. Your documents are human translated &
+          human approved.
+        </p>
       </div>
       <div className="space-y-2">
         <h1 className="text-xl text-bold text-cyan-900">Quality</h1>
-        <p>We are typically aware that mistranslation may cost you millions! Our robust quality assurance system makes us the most accurate translation company and helps you reduce costs.</p>
+        <p>
+          We are typically aware that mistranslation may cost you millions! Our
+          robust quality assurance system makes us the most accurate translation
+          company and helps you reduce costs.
+        </p>
       </div>
       <div className="space-y-2">
         <h1 className="text-xl text-bold text-cyan-900">Industry Experts</h1>
-        <p>A translation should not only be written well, but should be written in the language your industry understands. Our dedicated team of lawyer linguists have the knowledge and experience to convey messages using the typical language of law.</p>
+        <p>
+          A translation should not only be written well, but should be written
+          in the language your industry understands. Our dedicated team of
+          lawyer linguists have the knowledge and experience to convey messages
+          using the typical language of law.
+        </p>
       </div>
       <div className="space-y-2">
         <h1 className="text-xl text-bold text-cyan-900">Dedication</h1>
-        <p>APEX LEGAL team is dedicated to delivering the best services to meet all the needs of clients engaged in language related matters.</p>
+        <p>
+          APEX LEGAL team is dedicated to delivering the best services to meet
+          all the needs of clients engaged in language related matters.
+        </p>
       </div>
       <div className="space-y-2">
         <h1 className="text-xl text-bold text-cyan-900">Affordable Cost</h1>
-        <p>We have specially designed packages to address the translation needs of all our customers. Our affordable and competitive prices start from $0.05/word so that all the businesses can pursue their dream of going global.</p>
+        <p>
+          We have specially designed packages to address the translation needs
+          of all our customers. Our affordable and competitive prices start from
+          $0.05/word so that all the businesses can pursue their dream of going
+          global.
+        </p>
       </div>
       <div className="space-y-2">
-        <h1 className="text-xl text-bold text-cyan-900">Confidentiality and Privacy</h1>
-        <p>We always protect the confidentiality and privacy of your documents. All of our linguists sign non-disclosure agreements.</p>
+        <h1 className="text-xl text-bold text-cyan-900">
+          Confidentiality and Privacy
+        </h1>
+        <p>
+          We always protect the confidentiality and privacy of your documents.
+          All of our linguists sign non-disclosure agreements.
+        </p>
       </div>
     </section>
   )
@@ -72,30 +109,73 @@ function ContactUs() {
   return (
     <section className="h-screen flex flex-col justify-center items-center bg-cyan-900 text-cyan-100 space-y-8">
       <h2 className="text-3xl">Contact us</h2>
-      <form className="w-full" name="contact" method="POST" data-netlify-honeypot="honey" data-netlify-recaptcha="true" data-netlify="true">
+      <form
+        className="w-full"
+        name="contact"
+        method="POST"
+        data-netlify-honeypot="honey"
+        data-netlify-recaptcha="true"
+        data-netlify="true"
+      >
         <div className="hidden">
           <label htmlFor="honey">honey</label>
           <input type="text" name="honey" id="honey" />
         </div>
         <div className="md:w-1/2 lg:w-2/5 xl:w-1/3 2xl:w-1/4 space-y-3 mx-auto px-8">
           <div className="space-y-3">
-            <label className="block" htmlFor="name">Name <span className="text-red-500">*</span></label>
-            <input className="block w-full p-2 bg-stone-100 text-stone-900" type="text" name="name" id="name" required />
+            <label className="block" htmlFor="name">
+              Name <span className="text-red-500">*</span>
+            </label>
+            <input
+              className="block w-full p-2 bg-stone-100 text-stone-900"
+              type="text"
+              name="name"
+              id="name"
+              required
+            />
           </div>
           <div className="space-y-3">
-            <label className="block" htmlFor="email">Email <span className="text-red-500">*</span></label>
-            <input className="block w-full p-2 bg-stone-100 text-stone-900" type="email" name="email" id="email" required />
+            <label className="block" htmlFor="email">
+              Email <span className="text-red-500">*</span>
+            </label>
+            <input
+              className="block w-full p-2 bg-stone-100 text-stone-900"
+              type="email"
+              name="email"
+              id="email"
+              required
+            />
           </div>
           <div className="space-y-3">
-            <label className="block" htmlFor="company">Company</label>
-            <input className="block w-full p-2 bg-stone-100 text-stone-900" type="text" name="company" id="company" />
+            <label className="block" htmlFor="company">
+              Company
+            </label>
+            <input
+              className="block w-full p-2 bg-stone-100 text-stone-900"
+              type="text"
+              name="company"
+              id="company"
+            />
           </div>
           <div className="space-y-3">
-            <label className="block" htmlFor="message">Message <span className="text-red-500">*</span></label>
-            <textarea className="block w-full p-2 bg-stone-100 text-stone-900" name="message" rows={3} id="message" required />
+            <label className="block" htmlFor="message">
+              Message <span className="text-red-500">*</span>
+            </label>
+            <textarea
+              className="block w-full p-2 bg-stone-100 text-stone-900"
+              name="message"
+              rows={3}
+              id="message"
+              required
+            />
           </div>
           <div data-netlify-recaptcha="true"></div>
-          <button className="bg-stone-900 text-stone-100 px-4 py-2" type="submit">Send</button>
+          <button
+            className="bg-stone-900 text-stone-100 px-4 py-2"
+            type="submit"
+          >
+            Send
+          </button>
         </div>
       </form>
     </section>
