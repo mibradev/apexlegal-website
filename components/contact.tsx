30d7936b90a344d1dc2046ecb185916c5b525d07
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2022-12-28 20:29:22 +0200

    Update contact us section

---
+++ components/contact.tsx
@@ -0,0 +1,90 @@
+export default function Contact() {
+  return (
+    <>
+      <div className="flex items-center justify-center py-12 bg-sky-600">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
+              Contact us
+            </h2>
+          </div>
+          <form
+            name="contact"
+            method="POST"
+            data-netlify-honeypot="honey"
+            data-netlify-recaptcha="true"
+            data-netlify="true"
+          >
+            <div className="overflow-hidden">
+              <div className="hidden">
+                <label htmlFor="honey">honey</label>
+                <input type="text" name="honey" id="honey" />
+              </div>
+              <div className="space-y-6 px-4 py-5 sm:p-6">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-white"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="name"
+                    required
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-white"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    required
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-medium text-white"
+                  >
+                    Message
+                  </label>
+                  <div className="mt-1">
+                    <textarea
+                      id="message"
+                      name="message"
+                      rows={3}
+                      required
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"
+                      defaultValue={''}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div data-netlify-recaptcha="true"></div>
+              <div className="px-4 py-3 text-right sm:px-6">
+                <button
+                  type="submit"
+                  className="inline-flex justify-center rounded-md border border-transparent bg-white py-2 px-4 text-sm font-medium text-sky-600 shadow-sm hover:bg-sky-100 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+                >
+                  Send
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from '../components/hero'
 import Features from '../components/features'
 import Services from '../components/services'
 import About from '../components/about'
+import Contact from '../components/contact'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,84 +23,8 @@ const Home: NextPage = () => {
       <Features />
       <About />
       <Services />
+      <Contact />
     </div>
-  )
-}
-
-function ContactUs() {
-  return (
-    <section className="h-screen flex flex-col justify-center items-center bg-cyan-900 text-cyan-100 space-y-8">
-      <h2 className="text-3xl">Contact us</h2>
-      <form
-        className="w-full"
-        name="contact"
-        method="POST"
-        data-netlify-honeypot="honey"
-        data-netlify-recaptcha="true"
-        data-netlify="true"
-      >
-        <div className="hidden">
-          <label htmlFor="honey">honey</label>
-          <input type="text" name="honey" id="honey" />
-        </div>
-        <div className="md:w-1/2 lg:w-2/5 xl:w-1/3 2xl:w-1/4 space-y-3 mx-auto px-8">
-          <div className="space-y-3">
-            <label className="block" htmlFor="name">
-              Name <span className="text-red-500">*</span>
-            </label>
-            <input
-              className="block w-full p-2 bg-stone-100 text-stone-900"
-              type="text"
-              name="name"
-              id="name"
-              required
-            />
-          </div>
-          <div className="space-y-3">
-            <label className="block" htmlFor="email">
-              Email <span className="text-red-500">*</span>
-            </label>
-            <input
-              className="block w-full p-2 bg-stone-100 text-stone-900"
-              type="email"
-              name="email"
-              id="email"
-              required
-            />
-          </div>
-          <div className="space-y-3">
-            <label className="block" htmlFor="company">
-              Company
-            </label>
-            <input
-              className="block w-full p-2 bg-stone-100 text-stone-900"
-              type="text"
-              name="company"
-              id="company"
-            />
-          </div>
-          <div className="space-y-3">
-            <label className="block" htmlFor="message">
-              Message <span className="text-red-500">*</span>
-            </label>
-            <textarea
-              className="block w-full p-2 bg-stone-100 text-stone-900"
-              name="message"
-              rows={3}
-              id="message"
-              required
-            />
-          </div>
-          <div data-netlify-recaptcha="true"></div>
-          <button
-            className="bg-stone-900 text-stone-100 px-4 py-2"
-            type="submit"
-          >
-            Send
-          </button>
-        </div>
-      </form>
-    </section>
   )
 }
 
